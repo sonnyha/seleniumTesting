260e145fca0e94d8a963174abcde59d5bc9eb6eb
Ha <sha549@targaresources.com>
2023-02-27 09:19:29 -0600

    rough draft after encap

--- IMSTester.py
+++ IMSTester.py
@@ -1,6 +1,3 @@
-#   IMSTester is a module used for testing Inventory Management System (IMS)
-#   Testing Functions for IMS
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -9,6 +6,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+
+#   IMSTester is a module used for testing Inventory Management System (IMS)
+#   Testing Functions for IMS
+#   Still In Progress...
+
 def startIMSTest():
     url = "http://inventorymanagementdev:4431/"
     chrome_options = Options()
@@ -25,7 +27,6 @@ def headerCheck():
         print("Header Element Shown...")
     else:
         print("Header Element missing...")
-
 def goToMarketerBook():
     driver.find_element(By.PARTIAL_LINK_TEXT, "Marketer Book").click()
     WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, "begin_inventory_table")))
@@ -46,6 +47,50 @@ def checkDirectionTabs():
         Xpath = '//*[@id="gridDisplayTab"]/ul/li[' + str(i) + ']'
         driver.find_element(By.XPATH, Xpath).click()
         print("Marketer Tab Functional ", List1[i])
-        outputArray.append("Marketer Tab Functional " + List1[i])
         time.sleep(1)
+
+def marketerHeaderCheck():
+    HeaderDropDownList = ["Accounting Period Dropdown",
+                          "Inventory Pool DropDown",
+                          "Quantity Unit Dropdown",
+                          "Price Unit Dropdown"]
+    XpathDictionary = {
+        "Accounting Period Dropdown":
+            "//body[1]/div[1]/main[1]/section[1]/div[2]/div[1]/div[1]/span[1]/span[1]/span[2]/span[1]",
+        "Inventory Pool DropDown":
+            "//body[1]/div[1]/main[1]/section[1]/div[2]/div[1]/div[2]/span[1]/span[1]/span[2]/span[1]",
+        "Quantity Unit Dropdown":
+            "//body[1]/div[1]/main[1]/section[1]/div[2]/div[1]/div[3]/span[1]/span[1]/span[2]/span[1]",
+        "Price Unit Dropdown":
+            "//body[1]/div[1]/main[1]/section[1]/div[2]/div[1]/div[3]/span[2]/span[1]/span[2]/span[1]"
+    }
+
+    for dropdown in HeaderDropDownList:
+        driver.find_element(By.XPATH, XpathDictionary[dropdown]).click()
+        print("Testing " + dropdown)
+        time.sleep(.5)
+
+def refreshMarketerBook():
+    driver.find_element(By.ID, "RefreshButton").click()
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, "filter-controls")))
+    print("Testing Refresh button")
+
+def AccountingPeriodInceaseButton():
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, "filter-controls")))
+    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "periodIncrease")))
+    time.sleep(2)
+    button.click()
+    print("Testing Period Increase Button")
+
+def AccountingPeriodDeceaseButton():
+    WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, "filter-controls")))
+    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "periodDecrease")))
+    time.sleep(2)
+    button.click()
+    print("Testing Period Decrease Button")
+def output(Arr):
+    with open('outputFile.txt', 'w') as f:
+        for line in Arr:
+            f.write(line)
+            f.write('\n')
 
