429110c20fd65dae035fae447e56222ffe580970
Ha <sha549@targaresources.com>
2023-03-01 16:01:46 -0600

    main.py updated for testing

--- ExecuteTesting.py
+++ ExecuteTesting.py
@@ -1,33 +1,38 @@
 import AutoTesting
 import os
+import datetime
+
 class ExecuteTesting:
     logfile = ""
-    execution_status = ""
+    execution_status = "<Execution_Status Here>"
     execution_start = ""
     notification = []
+    errorCounter = 0
 
 #   Defines a new log file and execution status,
 #   the creates a new instance of AutoTesting and begins the testing process
     def launchAutoTesting(self):
         AutoTestingObj = AutoTesting.AutoTesting()
-        AutoTestingObj.initVariables()
+        AutoTestingObj.intializingVariables()
+        now = datetime.datetime.now()
+        self.execution_start = str(now)
         return AutoTestingObj
 
 #   Writes errors to the logfile and updates the execution_status on critical failure
-    def handlesError(self):
-        errorFound = False
-        if errorFound is True:
-            self.logfile = self.logfile + " error here"
-            self.execution_status = "Type of Error"
-            return "Error 42069"
+    def handlesError(self, result):
+        if result is True:
+            self.logfile = self.logfile + "Failed on Line 29"
+            self.execution_status = "Error Found"
+            self.notification.append("Web Element Not Found")
+            self.errorCounter = self.errorCounter + 1
+            return "Error ACA290x"
         return "No Errors"
 
 #   Provides a response to users defined in the notification list that the execution
-    #   has completed, the status of the test, andthe time/duration of the testing execution
+    #   has completed, the status of the test, and the time/duration of the testing execution
     def reportAutoTesting(self):
         print(
             "Testing Start Time: " + self.execution_start + os.linesep +
-            "Duration: " + " Minutes(End Time - Start Time)" + os.linesep +
-            "Num of Errors found: " + os.linesep +
-            "Types of Errors List Here"
+            "Num of Errors found: " + str(self.errorCounter) + os.linesep +
+            str(self.notification)
         )
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-import AutoTesting as AT
 import ExecuteTesting as ET
 
-test = ET.ExecuteTesting()
-print("hello world")
+print("------------------Testing Execute Testing------------------")
 
+# Simulating a test result that passes or fails
+dummy_test1 = True
+dummy_test2 = False
+test = ET.ExecuteTesting()
+
+print("------------------Object's Initial State------------------")
+# Object When called
+test.launchAutoTesting()
+print(test.logfile, test.execution_status, test.execution_start, test.notification)
+test.reportAutoTesting()
+
+print("------------------Object Passes Testing------------------")
+# Object when it passes testing
+test.handlesError(dummy_test1)
+print(test.logfile, test.execution_status, test.execution_start, test.notification)
+test.reportAutoTesting()
+
+print("------------------Object Fails Testing------------------")
+# Object when it fails testing
+test.handlesError(dummy_test2)
+print(test.logfile, test.execution_status, test.execution_start, test.notification)
+test.reportAutoTesting()
