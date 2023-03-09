d378e36a37578f7d874dd2b1e55845631607234f
Ha <sha549@targaresources.com>
2023-03-09 11:05:11 -0600

    try catch added

--- ExecuteTesting.py
+++ ExecuteTesting.py
@@ -9,6 +9,16 @@ class ExecuteTesting:
     notification = []
     errorCounter = 0
 
+    def errorTest(self, func, *args, **kwargs):
+        try:
+            # raise 10/0
+            func(*args, **kwargs)
+        except Exception as e:
+            print(f"Exception Type: {e}")
+            return e
+        # finally:
+        #     print("Executing finally clause")
+
 #   Defines a new log file and execution status,
 #   the creates a new instance of AutoTesting and begins the testing process
     def launchAutoTesting(self):
@@ -19,14 +29,14 @@ class ExecuteTesting:
         return AutoTestingObj
 
 #   Writes errors to the logfile and updates the execution_status on critical failure
-    def handlesError(self, result):
-        if result is True:
-            self.logfile = self.logfile + "Failed on Line 29"
-            self.execution_status = "Error Found"
-            self.notification.append("Web Element Not Found")
-            self.errorCounter = self.errorCounter + 1
-            return "Error ACA290x"
-        return "No Errors"
+#     def handlesError(self, result):
+#         if result is True:
+#             self.logfile = self.logfile + "Failed on Line 29"
+#             self.execution_status = "Error Found"
+#             self.notification.append("Web Element Not Found")
+#             self.errorCounter = self.errorCounter + 1
+#             return "Error ACA290x"
+#         return "No Errors"
 
 #   Provides a response to users defined in the notification list that the execution
     #   has completed, the status of the test, and the time/duration of the testing execution
@@ -36,3 +46,24 @@ class ExecuteTesting:
             "Num of Errors found: " + str(self.errorCounter) + os.linesep +
             str(self.notification)
         )
+
+
+
+#   Libraries needed
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+#
+# def handleError(driver, locator):
+#     try:
+#         # Find the web element using the given locator
+#         element = WebDriverWait(driver, 10).until(
+#             EC.presence_of_element_located(locator)
+#         )
+#         return element
+#     except NoSuchElementException as e:
+#         # This block will execute if the element is not found
+#         # Here, we print the exception message and return the exception object
+#         print(f"An exception occurred: {e}")
+#         return e
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import ExecuteTesting as ET
 
+
+# dummy function to test for failures
+def divisionByZero():
+    result = 1/0
+    return result
+
+
+# dummy functions to test for raising exceptions
+def this_passes():
+    result = 1/1
+    return result
+
+def charPlusInt():
+    result = 'c' + 1
+    return result
+
+
+def outOfIndex():
+    result = [1, 2]
+    return result[3]
+
 print("------------------Testing Execute Testing------------------")
 
 # Simulating a test result that passes or fails
@@ -7,21 +28,29 @@ dummy_test1 = True
 dummy_test2 = False
 test = ET.ExecuteTesting()
 
-print("------------------Object's Initial State------------------")
-# Object When called
-test.launchAutoTesting()
-print(test.logfile, test.execution_status, test.execution_start, test.notification)
-test.reportAutoTesting()
+test.errorTest(divisionByZero)
+test.errorTest(charPlusInt)
+test.errorTest(outOfIndex)
 
-print("------------------Object Passes Testing------------------")
-# Object when it passes testing
-test.handlesError(dummy_test1)
-print(test.logfile, test.execution_status, test.execution_start, test.notification)
-test.reportAutoTesting()
 
-print("------------------Object Fails Testing------------------")
-# Object when it fails testing
-test.handlesError(dummy_test2)
-print(test.logfile, test.execution_status, test.execution_start, test.notification)
-test.reportAutoTesting()
+
+
+# print("------------------Object's Initial State------------------")
+# # Object When called
+# test.launchAutoTesting()
+# print(test.logfile, test.execution_status, test.execution_start, test.notification)
+# test.reportAutoTesting()
+#
+# print("------------------Object Passes Testing------------------")
+# # Object when it passes testing
+# test.handlesError(dummy_test1)
+# print(test.logfile, test.execution_status, test.execution_start, test.notification)
+# test.reportAutoTesting()
+#
+# print("------------------Object Fails Testing------------------")
+# # Object when it fails testing
+# test.handlesError(dummy_test2)
+# print(test.logfile, test.execution_status, test.execution_start, test.notification)
+# test.reportAutoTesting()
+
 
